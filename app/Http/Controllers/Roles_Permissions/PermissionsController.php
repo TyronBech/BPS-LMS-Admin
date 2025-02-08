46ddda9cfda7af20b416e18570b572c397294355
Tyron Bechayda <tyronbechayda1112@gmail.com>
2025-02-08 14:13:49 +0800

    Add Permissions and Roles controllers, Admin model, and update authentication configuration

---
+++ app/Http/Controllers/Roles_Permissions/PermissionsController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\Roles_Permissions;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+
+class PermissionsController extends Controller
+{
+    //
+}
--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -2,15 +2,13 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Notifications\Notifiable;
 use Spatie\Permission\Traits\HasRoles;
 
-class User extends Authenticatable
+class Admin extends Authenticatable
 {
-    /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable, HasRoles;
 
     /**
