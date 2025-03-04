1ac865e10b32229df0cd75313df85c6a9644f7d0
Tyron Bechayda <tyronbechayda1112@gmail.com>
2025-03-04 17:38:40 +0800

    Add AdminSeeder and update RolePermissionSeeder for role and permission management

---
+++ database/seeders/AdminSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // $admin = new \App\Models\Admin();
+        // $admin->name = 'Tyron P. Bechayda';
+        // $admin->email = 'test@example.com';
+        // $admin->password = Hash::make('null');
+        // $admin->created_at = now();
+        // $admin->updated_at = now();
+        // $admin->save();
+        // $admin1 = new \App\Models\Admin();
+        // $admin1->name = 'Gecilie C. Almirañez';
+        // $admin1->email = 'sample@gmail.com';
+        // $admin1->password = Hash::make('null');
+        // $admin1->created_at = now();
+        // $admin1->updated_at = now();
+        // $admin1->save();
+    }
+}
--- database/seeders/RolePermissionSeeder.php
+++ database/seeders/RolePermissionSeeder.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Spatie\Permission\Models\Permission;
 use Spatie\Permission\Models\Role;
+use App\Models\Admin;
 
 class RolePermissionSeeder extends Seeder
 {
@@ -14,22 +15,29 @@ class RolePermissionSeeder extends Seeder
      */
     public function run(): void
     {
-        app()[\Spatie\Permission\PermissionRegistrar::class]->forgetCachedPermissions();
-        //Permission::create(['name' => 'Edit Users',       'guard_name' => 'admin']);
-        //Permission::create(['name' => 'Delete Users',     'guard_name' => 'admin']);
-        //Permission::create(['name' => 'Create Users',     'guard_name' => 'admin']);
-        //Permission::create(['name' => 'Modify Admins',    'guard_name' => 'admin']);
-        Permission::create(['name' => 'Create Books',    'guard_name' => 'admin']);
-        Permission::create(['name' => 'Edit Books',    'guard_name' => 'admin']);
-        Permission::create(['name' => 'Delete Books',    'guard_name' => 'admin']);
+        //app()[\Spatie\Permission\PermissionRegistrar::class]->forgetCachedPermissions();
+        // Permission::create(['name' => 'Edit Users',     'guard_name' => 'admin']);
+        // Permission::create(['name' => 'Delete Users',   'guard_name' => 'admin']);
+        // Permission::create(['name' => 'Create Users',   'guard_name' => 'admin']);
+        // Permission::create(['name' => 'Modify Admins',  'guard_name' => 'admin']);
+        // Permission::create(['name' => 'Create Books',   'guard_name' => 'admin']);
+        // Permission::create(['name' => 'Edit Books',     'guard_name' => 'admin']);
+        // Permission::create(['name' => 'Delete Books',   'guard_name' => 'admin']);
 
-        //$role = Role::create(['name' => 'Admin', 'guard_name' => 'admin']);
-        //$role->givePermissionTo('Edit Users', 'Create Users');
+        // $role1 = Role::create(['name' => 'Super Admin',  'guard_name' => 'admin']);
+        // $role2 = Role::create(['name' => 'Admin',        'guard_name' => 'admin']);
+        // $role3 = Role::create(['name' => 'Librarian',    'guard_name' => 'admin']);
+        // $role1->givePermissionTo('Edit Users', 'Create Users', 'Delete Users', 'Modify Admins', 'Create Books', 'Edit Books', 'Delete Books');
+        // $role2->givePermissionTo('Edit Users', 'Create Users', 'Delete Users', 'Create Books', 'Edit Books', 'Delete Books');
+        // $role3->givePermissionTo('Create Books', 'Edit Books', 'Delete Books');
         //$role = Role::findById(3, 'admin');
         //$permission = Permission::findById(3, 'admin');
         //$role->revokePermissionTo($permission);
         //$role->givePermissionTo('Edit Users', 'Create Users');
-        $role = Role::create(['name' => 'Librarian', 'guard_name' => 'admin']);
-        $role->givePermissionTo('Create Books', 'Edit Books', 'Delete Books');
+        //$role->givePermissionTo('Create Books', 'Edit Books', 'Delete Books');
+        // $admin1 = Admin::find(1);
+        // $admin1->assignRole('Super Admin');
+        // $admin2 = Admin::find(2);
+        // $admin2->assignRole('Admin');
     }
 }
