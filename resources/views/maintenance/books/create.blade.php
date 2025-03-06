cbcf23358a8c7118f225a34fe9439813cb145bf3
Tyron Bechayda <tyronbechayda1112@gmail.com>
2025-03-06 11:58:11 +0800

    Refactor book creation view to enhance styling and improve label consistency

--- resources/views/maintenance/books/create.blade.php
+++ resources/views/maintenance/books/create.blade.php
@@ -1,9 +1,9 @@
 @extends('layouts.admin-app')
 @section('content')
 <h1 class="font-semibold text-center text-4xl p-5">Maintenance</h1>
-<div class="w-full p-6 bg-white border border-gray-200 rounded-lg shadow">
+<div class="w-full p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
   <div class="flex justify-between">
-    <h5 class="mb-1 text-2xl font-bold tracking-tight text-gray-900">Add Book</h5>
+    <h5 class="mb-1 text-2xl font-bold tracking-tight">Add Book</h5>
     <a href="{{ route('maintenance.books') }}" class="inline-flex items-center px-3 py-1 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300">
       Back
       <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
@@ -16,7 +16,7 @@
     @csrf
     <h6 class="mb-1 text-xl font-semibold tracking-tight text-gray-800">Book Information</h6>
     <div class="mb-5">
-      <label for="accession" name="accession" class="block mb-2 text-sm font-medium text-gray-900">Accession Number:</label>
+      <label for="accession" name="accession" class="block mb-2 text-sm font-medium">Accession Number:</label>
       <input type="text" id="accession" name="accession" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="FIL0123456789">
       @error('accession')
       <div class="p-4 my-2 text-sm text-red-800 rounded-lg bg-red-50" role="alert">
@@ -25,7 +25,7 @@
       @enderror
     </div>
     <div class="mb-5">
-      <label for="call_number" name="call_number" class="block mb-2 text-sm font-medium text-gray-900">Call Number:</label>
+      <label for="call_number" name="call_number" class="block mb-2 text-sm font-medium">Call Number:</label>
       <input type="text" id="call_number" name="call_number" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="192.000...">
       @error('call_number')
       <div class="p-4 my-2 text-sm text-red-800 rounded-lg bg-red-50" role="alert">
@@ -34,7 +34,7 @@
       @enderror
     </div>
     <div class="mb-5">
-      <label for="title" name="title" class="block mb-2 text-sm font-medium text-gray-900">Title:</label>
+      <label for="title" name="title" class="block mb-2 text-sm font-medium">Title:</label>
       <input type="text" id="title" name="title" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="Title...">
       @error('title')
       <div class="p-4 my-2 text-sm text-red-800 rounded-lg bg-red-50" role="alert">
@@ -43,7 +43,7 @@
       @enderror
     </div>
     <div class="mb-5">
-      <label for="authors" name="authors" class="block mb-2 text-sm font-medium text-gray-900">Authors:</label>
+      <label for="authors" name="authors" class="block mb-2 text-sm font-medium">Authors:</label>
       <input type="text" id="authors" name="authors" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="Juan Dela Cruz">
       @error('authors')
       <div class="p-4 my-2 text-sm text-red-800 rounded-lg bg-red-50" role="alert">
@@ -52,7 +52,7 @@
       @enderror
     </div>
     <div class="mb-5">
-      <label for="edition" name="edition" class="block mb-2 text-sm font-medium text-gray-900">Edition:</label>
+      <label for="edition" name="edition" class="block mb-2 text-sm font-medium">Edition:</label>
       <input type="text" id="edition" name="edition" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="1st Edition">
       @error('edition')
       <div class="p-4 my-2 text-sm text-red-800 rounded-lg bg-red-50" role="alert">
@@ -61,7 +61,7 @@
       @enderror
     </div>
     <div class="mb-5">
-      <label for="publication" name="publication" class="block mb-2 text-sm font-medium text-gray-900">Publication:</label>
+      <label for="publication" name="publication" class="block mb-2 text-sm font-medium">Publication:</label>
       <input type="text" id="publication" name="publication" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="Manila, Philippines">
       @error('publication')
       <div class="p-4 my-2 text-sm text-red-800 rounded-lg bg-red-50" role="alert">
@@ -70,7 +70,7 @@
       @enderror
     </div>
     <div class="mb-5">
-      <label for="publisher" name="publisher" class="block mb-2 text-sm font-medium text-gray-900">Publisher:</label>
+      <label for="publisher" name="publisher" class="block mb-2 text-sm font-medium">Publisher:</label>
       <input type="text" id="publisher" name="publisher" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="National Library of the Philippines">
       @error('publisher')
       <div class="p-4 my-2 text-sm text-red-800 rounded-lg bg-red-50" role="alert">
@@ -79,7 +79,7 @@
       @enderror
     </div>
     <div class="mb-5">
-      <label for="copyright" name="copyright" class="block mb-2 text-sm font-medium text-gray-900">Copyright:</label>
+      <label for="copyright" name="copyright" class="block mb-2 text-sm font-medium">Copyright:</label>
       <input type="text" id="copyright" name="copyright" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="2026">
       @error('copyright')
       <div class="p-4 my-2 text-sm text-red-800 rounded-lg bg-red-50" role="alert">
@@ -88,7 +88,7 @@
       @enderror
     </div>
     <div class="mb-5">
-      <label for="cover_image" name="cover_image" class="block mb-2 text-sm font-medium text-gray-900">Cover Image:</label>
+      <label for="cover_image" name="cover_image" class="block mb-2 text-sm font-medium">Cover Image:</label>
       <input class="block w-full text-sm text-gray-600 border border-gray-900 rounded-md cursor-pointer bg-gray-100 focus:outline-none" id="cover_image" name="cover_image" type="file">
       @error('cover_image')
       <div class="p-4 my-2 text-sm text-red-800 rounded-lg bg-red-50" role="alert">
@@ -97,7 +97,7 @@
       @enderror
     </div>
     <div class="mb-5">
-      <label for="digital_copy_url" name="digital_copy_url" class="block mb-2 text-sm font-medium text-gray-900">Digital Copy URL:</label>
+      <label for="digital_copy_url" name="digital_copy_url" class="block mb-2 text-sm font-medium">Digital Copy URL:</label>
       <input type="text" id="digital_copy_url" name="digital_copy_url" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="www.example.com">
       @error('digital_copy_url')
       <div class="p-4 my-2 text-sm text-red-800 rounded-lg bg-red-50" role="alert">
@@ -106,7 +106,7 @@
       @enderror
     </div>
     <div class="mb-5">
-      <label for="remarks" name="remarks" class="block mb-2 text-sm font-medium text-gray-900">Select Remarks:</label>
+      <label for="remarks" name="remarks" class="block mb-2 text-sm font-medium">Select Remarks:</label>
       <select id="remarks" name="remarks" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5">
         <option selected>Onshelf</option>
         <option>Lost</option>
