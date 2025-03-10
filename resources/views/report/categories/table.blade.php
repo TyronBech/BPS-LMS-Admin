242793563d743be79045856ad49cfae615eda006
TyronBech <tyronbechayda1112@gmail.com>
2025-03-10 18:04:39 +0800

    Update report table header and add total row for materials inventory

--- resources/views/report/categories/table.blade.php
+++ resources/views/report/categories/table.blade.php
@@ -1,5 +1,5 @@
 <div class="container flex flex-col border-collapse border-2 overflow-x-auto border-slate-900 mt-2 mb-4 rounded-lg bg-white dark:bg-gray-800 dark:border-gray-600">
-  <h2 class="text-center mb-4 mt-4 font-semibold text-2xl">Report Table for Users</h2>
+  <h2 class="text-center mb-4 mt-4 font-semibold text-2xl">Report Table for Materials</h2>
   <table class="table-fixed m-4 bg-white dark:bg-gray-800">
     <thead id="today-header" class="bg-blue-400 text-center font-bold text-slate-200">
       <th>Legend</th>
@@ -12,18 +12,25 @@
     <tbody id="students-activity">
       @forelse($data as $item)
         <tr class="text-center">
-          <td class="pb-1 text-center">{{ $item->legend }}</td>
-          <td class="pb-1 text-center">{{ $item->name }}</td>
+          <td class="pb-1">{{ $item->legend }}</td>
+          <td class="pb-1 text-center border-r">{{ $item->name }}</td>
           <td class="pb-1 text-center">{{ $item->previous_inventory }}</td>
           <td class="pb-1 text-center">{{ $item->newly_acquired }}</td>
           <td class="pb-1 text-center">{{ $item->discarded }}</td>
           <td class="pb-1 text-center">{{ $item->present_inventory }}</td>
         </tr>
-      @empty
+        @empty
         <tr>
           <td colspan="8" class="text-center">No data found.</td>
         </tr>
-      @endforelse
+        @endforelse
+        <tr class="border-t">
+          <td class="pb-1 text-right border-r pr-3" colspan="2">Total:</td>
+          <td class="pb-1 text-center">{{ $data->sum('previous_inventory') }}</td>
+          <td class="pb-1 text-center">{{ $data->sum('newly_acquired') }}</td>
+          <td class="pb-1 text-center">{{ $data->sum('discarded') }}</td>
+          <td class="pb-1 text-center">{{ $data->sum('present_inventory') }}</td>
+        </tr>
     </tbody>
   </table>
 </div>
