714e30520735b48acbf72e713a7d1b580c3071d8
TyronBech <tyronbechayda1112@gmail.com>
2025-03-17 17:24:30 +0800

    Update inventory view title to 'Book Inventory' for clarity

--- resources/views/Inventory/inventory.blade.php
+++ resources/views/Inventory/inventory.blade.php
@@ -4,7 +4,7 @@
 <div class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow-sm mb-12 md:flex-row md:max-w-xl dark:border-gray-700 dark:bg-gray-800">
   <img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg" src="{{ asset('img/books.png') }}" id="book-img" alt="Book Image">
   <div class="flex flex-col justify-between p-4 leading-normal">
-    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Book Title</h5>
+    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Book Inventory</h5>
     <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Scan the book or type it below.</p>
     <form action="{{ route('inventory.search') }}" id="search-form" method="POST" class="flex flex-col items-center">
       @csrf
