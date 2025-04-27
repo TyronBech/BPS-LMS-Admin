6ba712c90e2e836271b65a5609bf7471d7955fc4
TyronBech <tyronbechayda1112@gmail.com>
2025-04-27 22:39:50 +0800

    Update dashboard: change header from 'Recorded Books' to 'Total Books' for clarity

--- resources/views/dashboard/dashboard.blade.php
+++ resources/views/dashboard/dashboard.blade.php
@@ -31,7 +31,7 @@
   </div>
   <div class="flex flex-col min-h-96 justify-between max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700">
     <div>
-      <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Recorded Books</h5>
+      <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Total Books</h5>
       <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Total number of books in the database.</p>
     </div>
     <div>
