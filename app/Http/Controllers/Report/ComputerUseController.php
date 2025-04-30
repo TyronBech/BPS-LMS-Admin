5468b669136968f660d33aff0f298342f986cd2e
TyronBech <tyronbechayda1112@gmail.com>
2025-05-01 01:27:44 +0800

    Add Computer Use report functionality with search and export options

---
+++ app/Http/Controllers/Report/ComputerUseController.php
@@ -0,0 +1,161 @@
+<?php
+
+namespace App\Http\Controllers\Report;
+use App\Http\Controllers\Controller;
+
+use Illuminate\Http\Request;
+use App\Models\Log; // Import the Log model
+use Illuminate\Support\Facades\DB; // Import the DB facade
+use Carbon\Carbon; // Import the Carbon class
+use Illuminate\Support\Facades\Validator; // Import the Validator facade
+use DateTime; // Import the DateTime class
+use Barryvdh\DomPDF\Facade\Pdf; // Import the Pdf facade
+use PhpOffice\PhpSpreadsheet\Spreadsheet; // Import the Spreadsheet class
+use PhpOffice\PhpSpreadsheet\Writer\Xlsx as WriterXlsx; // Import the WriterXlsx class with alias
+
+class ComputerUseController extends Controller
+{
+    public function index()
+    {
+        $inputName      = null;
+        $fromInputDate  = null;
+        $toInputDate    = null;
+        $peak_hour      = "00:00";
+        $data           = Log::with('users')->where('computer_use', 'Yes')
+            ->orderBy(DB::raw('date(timestamp)'), 'desc')
+            ->orderBy(DB::raw('time(timestamp)'), 'desc')->get();
+        $hours = $data->map(function ($item) {
+            $item = Carbon::parse($item->timestamp)->format('H:i:s');
+            return $item;
+        });
+        $hour = $this->findPeakHour($hours);
+        if ($hour == 12) {
+            $peak_hour = "12:00 PM";
+        } else if ($hour == 0) {
+            $peak_hour = "12:00 AM";
+        } else if ($hour > 12) {
+            $peak_hour = $hour - 12 . ":00 PM";
+        } else {
+            $peak_hour = $hour . ":00 AM";
+        }
+        return view('report.computers.index', compact('data', 'inputName', 'fromInputDate', 'toInputDate', 'peak_hour'));
+    }
+    public function search(Request $request)
+    {
+        $inputName      = $request->input('first-name');
+        $fromInputDate  = $request->input('start');
+        $toInputDate    = $request->input('end');
+        $peak_hour      = "00:00";
+        $validator = Validator::make($request->all(), [
+            'start'         => 'sometimes',
+            'end'           => 'sometimes',
+            'last-name'     => 'sometimes',
+            'first-name'    => 'sometimes',
+        ]);
+        if ($validator->fails()) {
+            return redirect()->back()->with('toast-warning', $validator->errors()->first());
+        }
+        if ($request->input('submit') == 'pdf') {
+            $data = $this->generateData($request);
+            $this->generatePDF($data);
+            return redirect()->route('report.computer-use')->with('toast-success', 'Successfully exported to PDF');
+        } else if ($request->input('submit') == 'excel') {
+            $data = $this->generateData($request);
+            $this->exportExcel($data);
+            return redirect()->route('report.computer-use')->with('toast-success', 'Successfully exported to Excel');
+        }
+        $data = $this->generateData($request);
+        $hours = $data->map(function ($item) {
+            $item = Carbon::parse($item->timestamp)->format('H:i:s');
+            return $item;
+        });
+        $hour = $this->findPeakHour($hours);
+        if ($hour == 12) {
+            $peak_hour = "12:00 PM";
+        } else if ($hour == 0) {
+            $peak_hour = "12:00 AM";
+        } else if ($hour > 12) {
+            $peak_hour = $hour - 12 . ":00 PM";
+        } else {
+            $peak_hour = $hour . ":00 AM";
+        }
+        return view('report.computers.index', compact('data', 'inputName', 'fromInputDate', 'toInputDate', 'peak_hour'));
+    }
+    private function findPeakHour($times)
+    {
+        $hourCounts = array();
+        foreach ($times as $time) {
+            $hour = substr($time, 0, 2);
+            $hourCounts[$hour] = isset($hourCounts[$hour]) ? $hourCounts[$hour] + 1 : 1;
+        }
+        if (count($hourCounts) == 0) return "00";
+        $maxCount = 0;
+        foreach ($hourCounts as $hour => $count) {
+            if ($count > $maxCount) {
+                $maxCount = $count;
+                $peakHour = $hour;
+            }
+        }
+        return $peakHour;
+    }
+    private function generatePDF($data)
+    {
+        $chunk      = $data->chunk(25);
+        $arrayPdf   = array('data' => $chunk);
+        $pdf        = Pdf::loadView('pdf.user-pdf-report-format', $arrayPdf);
+        $directory  = 'C:/Users/tyron/Downloads';
+        $pdf->save($directory . '/computer-use-report_' . date('Y-m-d') . '.pdf');
+    }
+    private function exportExcel($data)
+    {
+        $spreadsheet    = new Spreadsheet();
+        $sheet          = $spreadsheet->getActiveSheet();
+        $sheet->setCellValue('A1', 'Name');
+        $sheet->setCellValue('B1', 'Date');
+        $sheet->setCellValue('C1', 'Time');
+        $sheet->setCellValue('D1', 'Compute Use');
+        $sheet->setCellValue('E1', 'Action');
+        $row = 2;
+        foreach ($data as $item) {
+            if(!$item->users) {
+                continue; // Skip if users relationship is not loaded
+            }
+            $sheet->setCellValue('A' . $row, $item->users->last_name . ', ' . $item->users->first_name . ' ' . $item->users->middle_name);
+            $sheet->setCellValue('B' . $row, Carbon::parse($item->timestamp)->format('Y-m-d'));
+            $sheet->setCellValue('C' . $row, Carbon::parse($item->timestamp)->format('H:i:s'));
+            $sheet->setCellValue('D' . $row, $item->computer_use);
+            $sheet->setCellValue('E' . $row, $item->action);
+            $row++;
+        }
+        $writer     = new WriterXlsx($spreadsheet);
+        $directory  = 'C:/Users/tyron/Downloads';
+        $filename   = $directory . '/computer-use-report_' . date('Y-m-d') . '.xlsx';
+        $writer->save($filename);
+    }
+    private function generateData(Request $request)
+    {
+        $fromInputDate  = $request->input('start');
+        $toInputDate    = $request->input('end');
+        $inputName      = strtolower($request->input('first-name'));
+
+        $query = Log::with('users');
+
+        if (strlen($fromInputDate) > 0) {
+            $fromInputDate = DateTime::createFromFormat('m/d/Y', $fromInputDate)->format('Y-m-d');
+            $toInputDate = DateTime::createFromFormat('m/d/Y', $toInputDate)->format('Y-m-d');
+            $query->whereBetween(DB::raw('DATE(log_user_logs.timestamp)'), [$fromInputDate, $toInputDate]); // Corrected table name
+        }
+
+        if (strlen($inputName) > 0) {
+            $query->whereHas('users', function ($q) use ($inputName) {
+                $q->where(DB::raw('lower(first_name)'), 'like', '%' . $inputName . '%')
+                    ->orWhere(DB::raw('lower(last_name)'), 'like', '%' . $inputName . '%')
+                    ->orWhere(DB::raw('lower(middle_name)'), 'like', '%' . $inputName . '%');
+            });
+        }
+
+        $data = $query->where('computer_use', 'Yes')->orderBy(DB::raw('DATE(log_user_logs.timestamp)'), 'asc') // Corrected table name
+            ->get();
+        return $data;
+    }
+}
---
+++ resources/views/report/computers/table.blade.php
@@ -0,0 +1,25 @@
+<div class="container flex flex-col border-collapse border-2 overflow-x-auto border-slate-900 mt-2 mb-4 rounded-lg bg-white dark:bg-gray-800 dark:border-gray-600">
+  <h2 class="text-center mb-4 mt-4 font-semibold text-2xl">Report Table for Computer Use</h2>
+  <table class="table-fixed m-4 bg-white dark:bg-gray-800">
+    <thead id="today-header" class="bg-blue-400 text-center font-bold text-slate-200">
+      <th>Name</th>
+      <th>Date</th>
+      <th>Time</th>
+    </thead>
+    <tbody id="students-activity">
+      @forelse($data as $item)
+      @if($item->users)
+      <tr class="text-center">
+        <td class="pb-1 min-w-20">{{ $item->users->last_name ? $item->users->last_name : '-' }}, {{ $item->users->first_name }} {{ $item->users->middle_name }}</td>
+        <td class="pb-1">{{ \Carbon\Carbon::parse($item->timestamp)->format('Y-m-d') }}</td>
+        <td class="pb-1">{{ \Carbon\Carbon::parse($item->timestamp)->format('H:i:s') }}</td>
+      </tr>
+      @endif
+      @empty
+      <tr>
+        <td colspan="8" class="text-center">No data found.</td>
+      </tr>
+      @endforelse
+    </tbody>
+  </table>
+</div>
