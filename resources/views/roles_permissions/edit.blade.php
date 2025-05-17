51d0722dfa295fd4156bde26f5358ec4d58a4936
TyronBech <tyronbechayda1112@gmail.com>
2025-05-18 02:38:38 +0800

    Enhance role input field styling for better dark mode support

--- resources/views/roles_permissions/edit.blade.php
+++ resources/views/roles_permissions/edit.blade.php
@@ -25,7 +25,7 @@
         <p>{{ $role->name }}</p>
       </div>
       @else
-      <input type="text" id="role" name="role" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="Admin" value="{{ $role->name }}">
+      <input type="text" id="role" name="role" class="bg-gray-50 border border-gray-300 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Admin" value="{{ $role->name }}">
       @endif
       @error('role')
       <div class="p-4 my-2 text-sm text-red-800 rounded-lg bg-red-50" role="alert">
