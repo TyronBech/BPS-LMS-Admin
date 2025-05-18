01a5ac87794336f10774c91f97a63a7c24dbc445
TyronBech <tyronbechayda1112@gmail.com>
2025-05-19 00:59:04 +0800

    Update user_id and book_id ranges in TransactionFactory for improved data generation

--- database/factories/TransactionFactory.php
+++ database/factories/TransactionFactory.php
@@ -17,8 +17,8 @@ class TransactionFactory extends Factory
     public function definition(): array
     {
         return [
-            'user_id' => $this->faker->randomElement([5, 6, 8, 9, 17, 18, 19]),
-            'book_id' => $this->faker->numberBetween(3, 46),
+            'user_id' => $this->faker->randomElement([5, 6, 8, 9, 19, 21, 22, 24, 25, 27]),
+            'book_id' => $this->faker->numberBetween(47, 89),
             'transaction_type' => $this->faker->randomElement(['Borrowed', 'Returned', 'Reserved']),
             'date_borrowed' => $this->faker->dateTimeThisYear(),
             'due_date' => $this->faker->dateTimeThisYear(),
