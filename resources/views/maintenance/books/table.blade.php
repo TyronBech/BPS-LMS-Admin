1bb8d178b00c1a4699d350ca7a286a63e4fc15fb
TyronBech <tyronbechayda1112@gmail.com>
2025-06-02 00:36:12 +0800

    Refactor delete functionality for admins, books, employees, and students to use modal confirmation and streamline form submissions

--- resources/views/maintenance/books/table.blade.php
+++ resources/views/maintenance/books/table.blade.php
@@ -46,11 +46,8 @@ $bookID = null;
             @can(PermissionsEnum::EDIT_BOOKS, 'admin')
               <a href="{{ route('maintenance.edit-book', ['id' => $item->id]) }}" id="editBtn" name="editBtn" class="text-white bg-blue-500 hover:bg-blue-700 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2 me-2 my-2">Edit</a>
             @endcan
-            @php
-            $bookID = ['id' => $item->id];
-            @endphp
             @can(PermissionsEnum::DELETE_BOOKS, 'admin')
-              <button data-modal-target="popup-modal" data-modal-toggle="popup-modal" class="focus:outline-none text-white bg-red-500 hover:bg-red-700 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2 me-2 my-2" type="button">
+              <button data-modal-target="delete-book-modal" data-modal-toggle="delete-book-modal" class="deleteBookBtn focus:outline-none text-white bg-red-500 hover:bg-red-700 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2 me-2 my-2" type="button" value="{{ $item->id }}">
                 Delete
               </button>
             @endcan
@@ -65,10 +62,10 @@ $bookID = null;
     </table>
   </div>
 </div>
-<div id="popup-modal" tabindex="-1" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
+<div id="delete-book-modal" tabindex="-1" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
   <div class="relative p-4 w-full max-w-md max-h-full">
     <div class="relative bg-white rounded-lg shadow-sm dark:bg-gray-700">
-      <button type="button" class="absolute top-3 end-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="popup-modal">
+      <button type="button" class="absolute top-3 end-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="delete-book-modal">
         <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
           <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
         </svg>
@@ -79,15 +76,26 @@ $bookID = null;
           <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 11V6m0 8h.01M19 10a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
         </svg>
         <h3 class="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">Are you sure you want to delete this book?</h3>
-        <form action="{{ route('maintenance.delete-book', $bookID) }}" method="POST" class="flex items-center justify-center">
+        <form action="{{ route('maintenance.delete-book') }}" method="POST" class="flex items-center justify-center">
           @csrf
           @method('DELETE')
-          <button data-modal-hide="popup-modal" type="submit" class="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center">
+          <input type="hidden" name="id" id="delete_book_id" value="" />
+          <button data-modal-hide="delete-book-modal" type="submit" class="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center">
             Yes, I'm sure
           </button>
-          <button data-modal-hide="popup-modal" type="button" class="py-2.5 px-5 ms-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">No, cancel</button>
+          <button data-modal-hide="delete-book-modal" type="button" class="py-2.5 px-5 ms-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">No, cancel</button>
         </form>
       </div>
     </div>
   </div>
 </div>
+<script>
+  const deleteBookBtn = document.querySelectorAll('.deleteBookBtn');
+  const deleteBookID = document.getElementById('delete_book_id');
+  deleteBookBtn.forEach(btn => {
+    btn.addEventListener('click', function(event) {
+      const bookId = event.target.value;
+      deleteBookID.value = bookId;
+    });
+  });
+</script>
