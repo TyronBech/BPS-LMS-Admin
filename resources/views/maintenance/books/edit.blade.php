8dda998e79ed11fe3b52a08bcfacddd5c7bf9c77
TyronBech <tyronbechayda1112@gmail.com>
2025-06-02 23:33:07 +0800

    Fix book description textarea value binding and set selected option for book type

--- resources/views/maintenance/books/edit.blade.php
+++ resources/views/maintenance/books/edit.blade.php
@@ -54,7 +54,7 @@
     </div>
     <div class="mb-5">
       <label for="description" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Book Description:</label>
-      <textarea id="description" name="description" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Write book description here..." value="{{ $book->description }}"></textarea>
+      <textarea id="description" name="description" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Write book description here...">{{ $book->description ?? '' }}</textarea>
       @error('description')
       <div class="p-4 my-2 text-sm text-red-800 rounded-lg bg-red-50" role="alert">
         <span class="font-medium">{{ $message }}</span>
@@ -137,7 +137,11 @@
       <label for="book_type" class="block mb-2 text-sm font-medium">Select book type:</label>
       <select id="book_type" name="book_type" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
         @foreach($book_types as $value)
+        @if($value == $book->book_type)
+        <option value="{{ $value }}" selected>{{ $value }}</option>
+        @else
         <option value="{{ $value }}">{{ $value }}</option>
+        @endif
         @endforeach
       </select>
       @error('book_type')
