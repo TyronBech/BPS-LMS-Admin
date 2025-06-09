9f7857d66aebc3316983e98e2b127a45c4121131
TyronBech <tyronbechayda1112@gmail.com>
2025-06-09 20:31:19 +0800

    Update logo alt text in email header for consistency

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -3,7 +3,7 @@
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
 @if (trim($slot) === 'Laravel')
-<img src="{{ asset('img/BPSLogo.png') }}" class="logo" alt="Laravel Logo">
+<img src="{{ asset('img/BPSLogo.png') }}" class="logo" alt="BPS Logo">
 @else
 {{ $slot }}
 @endif
