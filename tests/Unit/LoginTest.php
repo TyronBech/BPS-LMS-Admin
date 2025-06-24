734e7eb15c0f8eb0d094c90936334311035e60d2
TyronBech <tyronbechayda1112@gmail.com>
2025-06-24 21:00:49 +0800

    Remove legacy authentication tests and example test files; add a new basic unit test for login functionality.

---
+++ tests/Unit/LoginTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Tests\Unit;
+
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use PHPUnit\Framework\TestCase;
+
+class LoginTest extends TestCase
+{
+    use DatabaseTransactions;
+    /**
+     * A basic unit test example.
+     */
+    public function test_example(): void
+    {
+        $this->assertTrue(true);
+    }
+}
