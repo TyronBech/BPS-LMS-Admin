00c5dff915e5f64d2746e40d92bbfc8dbcb5f6cf
TyronBech <tyronbechayda1112@gmail.com>
2025-08-09 00:13:44 +0800

    Remove all unused imports

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,10 +1,6 @@
-import './bootstrap';
 import 'flowbite';
 import Chart from 'chart.js/auto';
-import Alpine from 'alpinejs';
 import ChartDataLabels from 'chartjs-plugin-datalabels';
 
 window.Chart = Chart;
-window.Alpine = Alpine;
 Chart.register(ChartDataLabels);
-Alpine.start();
