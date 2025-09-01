35cf7d31da2828bd338371d27c0ad4775d3bac0a
TyronBech <tyronbechayda1112@gmail.com>
2025-09-01 21:22:05 +0800

    Add jQuery import and assign to global window object for accessibility in app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,8 @@
 import 'flowbite';
 import Chart from 'chart.js/auto';
 import ChartDataLabels from 'chartjs-plugin-datalabels';
+import $ from 'jquery';
 
+window.$ = window.jQuery = $;
 window.Chart = Chart;
 Chart.register(ChartDataLabels);
