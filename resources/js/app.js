100ec27f99a4303004dabadfe8438d3014c98947
TyronBech <tyronbechayda1112@gmail.com>
2025-09-01 21:23:02 +0800

    Add axios import and configure default headers for AJAX requests in app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,7 +2,10 @@ import 'flowbite';
 import Chart from 'chart.js/auto';
 import ChartDataLabels from 'chartjs-plugin-datalabels';
 import $ from 'jquery';
+import axios from 'axios';
 
+window.axios = axios;
+window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 window.$ = window.jQuery = $;
 window.Chart = Chart;
 Chart.register(ChartDataLabels);
