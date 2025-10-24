3362fc93604d84c4c3b82b9463172ecfaba3b0fe
TyronBech <tyronbechayda1112@gmail.com>
2025-10-24 19:57:57 +0800

    Add skip-loader class to backup action buttons and implement delete confirmation modal in backup table

--- resources/views/layouts/welcome-layout-header.blade.php
+++ resources/views/layouts/welcome-layout-header.blade.php
@@ -1,26 +1,32 @@
 <header class="sticky top-0 z-50">
-  <nav class="bg-bpsBlue border-gray-200 dark:bg-bpsBlue min-h-36 pt-5">
-    <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto">
+  <nav class="bg-bpsBlue border-gray-200 dark:bg-bpsBlue">
+    <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
       <a href="{{ route('main-welcome') }}" class="flex items-center space-x-3 rtl:space-x-reverse">
-        <img class="rounded-full w-24 h-24" src="{{ asset('img/BPSLogo.png') }}" alt="School Logo">
+        <img class="rounded-full w-16 h-16 md:w-24 md:h-24" src="{{ asset('img/BPSLogo.png') }}" alt="School Logo">
         <div class="flex flex-col justify-center">
-          <h1 class="lg:text-2xl md:text-lg text-white font-semibold text-center dark:text-white">Bicutan Parochial School, Inc.</h1>
-          <hr class="h-px bg-gray-200 border-0 dark:bg-white">
-          <h1 class="lg:text-xl md:text-md text-white font-semibold text-center dark:text-white">Library Management System</h1>
+          <h1 class="text-md sm:text-lg md:text-xl lg:text-2xl text-white font-semibold dark:text-white">Bicutan Parochial School, Inc.</h1>
+          <hr class="h-px my-1 bg-gray-200 border-0 dark:bg-white">
+          <h1 class="text-sm sm:text-base md:text-lg lg:text-xl text-white font-semibold dark:text-white">Library Management System</h1>
         </div>
       </a>
-      <div class="hidden w-full lg:block lg:w-auto pt-4" id="navbar-dropdown">
-        <ul class="flex flex-col font-medium p-4 lg:p-0 mt-4 mx-8 mb-4 border border-gray-100 rounded-lg bg-bpsBlue lg:space-x-8 rtl:space-x-reverse lg:flex-row lg:mt-0 lg:border-0 lg:bg-bpsBlue dark:bg-bpsBlue">
+      <button data-collapse-toggle="navbar-dropdown" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-100 rounded-lg lg:hidden hover:bg-gray-100/20 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-dropdown" aria-expanded="false">
+        <span class="sr-only">Open main menu</span>
+        <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+          <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15" />
+        </svg>
+      </button>
+      <div class="hidden w-full lg:block lg:w-auto" id="navbar-dropdown">
+        <ul class="flex flex-col font-medium p-4 lg:p-0 mt-4 border border-gray-100 rounded-lg bg-bpsBlue lg:flex-row lg:space-x-8 rtl:space-x-reverse lg:mt-0 lg:border-0 lg:bg-bpsBlue dark:bg-bpsBlue lg:dark:bg-bpsBlue dark:border-gray-700">
           <li>
-            <a href="#" class="block py-2 px-3 text-white bg-bpsBlue rounded hover:underline lg:bg-transparent lg:text-white lg:p-0" aria-current="page">Services</a>
+            <a href="#" class="block py-2 px-3 text-white rounded hover:bg-gray-100/20 lg:hover:bg-transparent lg:border-0 lg:hover:text-blue-100 lg:p-0 dark:text-white lg:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent" aria-current="page">Services</a>
           </li>
           <li>
-            <a href="#" class="block py-2 px-3 text-white rounded hover:underline lg:hover:bg-transparent lg:border-0 lg:hover:text-blue-100 lg:p-0">About</a>
+            <a href="#" class="block py-2 px-3 text-white rounded hover:bg-gray-100/20 lg:hover:bg-transparent lg:border-0 lg:hover:text-blue-100 lg:p-0 dark:text-white lg:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent">About</a>
           </li>
           <li>
             <form action="{{ route('login') }}" method="GET">
               @csrf
-              <button class="block py-2 px-3 text-white rounded hover:underline lg:hover:bg-transparent lg:border-0 lg:hover:text-blue-100 lg:p-0" type="submit">
+              <button class="block w-full text-left py-2 px-3 text-white rounded hover:bg-gray-100/20 lg:hover:bg-transparent lg:border-0 lg:hover:text-blue-100 lg:p-0 dark:text-white lg:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent" type="submit">
                 Login
               </button>
             </form>
