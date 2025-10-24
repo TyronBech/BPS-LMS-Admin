8e7433fb43348e68a4281e62d80358be0b22d0aa
TyronBech <tyronbechayda1112@gmail.com>
2025-10-24 11:04:02 +0800

    Enhance user profile display in admin header with dynamic image retrieval and fallback; improve dropdown button styling for better user experience.

--- resources/views/layouts/admin-header.blade.php
+++ resources/views/layouts/admin-header.blade.php
@@ -17,7 +17,7 @@
         </svg>
       </button>
       <div class="hidden w-full lg:block lg:w-auto" id="navbar-dropdown">
-        <ul class="flex flex-col font-medium p-4 lg:p-0 mt-4 border border-gray-100 rounded-lg bg-bpsBlue lg:flex-row lg:space-x-8 rtl:space-x-reverse lg:mt-0 lg:border-0 lg:bg-bpsBlue dark:bg-gray-800 lg:dark:bg-bpsBlue dark:border-gray-700">
+        <ul class="flex flex-col font-medium p-4 lg:p-0 mt-4 border border-gray-100 rounded-lg bg-bpsBlue lg:flex-row lg:items-center lg:space-x-8 rtl:space-x-reverse lg:mt-0 lg:border-0 lg:bg-bpsBlue dark:bg-gray-800 lg:dark:bg-bpsBlue dark:border-gray-700">
           <li>
             <a href="{{ route('dashboard') }}" class="block py-2 px-3 text-white rounded hover:bg-blue-800 lg:hover:bg-transparent lg:border-0 lg:hover:text-blue-300 lg:p-0 dark:text-white lg:dark:hover:text-blue-400 dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent" aria-current="page">Home</a>
           </li>
@@ -291,9 +291,31 @@
           </li>
           @endif
           <li>
-            <button id="dropdownNavbarLink" data-dropdown-toggle="userSettings" class="flex items-center justify-between w-full py-2 px-3 text-white rounded hover:bg-blue-800 lg:hover:bg-transparent lg:border-0 lg:hover:text-blue-300 lg:p-0 lg:w-auto dark:text-white lg:dark:hover:text-blue-400 dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent">{{ auth()->user()->first_name }} <svg class="w-2.5 h-2.5 ms-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+            <?php
+
+            use Illuminate\Support\Facades\Auth;
+
+            $base64Image = Auth::user()->profile_image;
+            if (!empty($base64Image)) {
+              $imageData = base64_decode($base64Image);
+
+              // Detect MIME type
+              $finfo = finfo_open(FILEINFO_MIME_TYPE);
+              $mimeType = finfo_buffer($finfo, $imageData);
+              finfo_close($finfo);
+            }
+            ?>
+            <button id="dropdownNavbarLink" data-dropdown-toggle="userSettings" class="flex items-center text-white rounded hover:bg-blue-800 lg:hover:bg-transparent lg:border-0 lg:hover:text-blue-300 lg:p-0 dark:text-white lg:dark:hover:text-blue-400 dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent">
+              @if(!empty($base64Image))
+              <img class="w-8 h-8 rounded-full" src="data:{{ $mimeType }};base64,{{ $base64Image }}" alt="{{ auth()->user()->first_name }} image">
+              @else
+              <img class="w-8 h-8 rounded-full" src="{{ asset('img/User-dark.png') }}" alt="{{ auth()->user()->first_name }} image">
+              @endif
+              <span class="ms-2">{{ auth()->user()->first_name }}</span>
+              <svg class="w-2.5 h-2.5 ms-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4" />
-              </svg></button>
+              </svg>
+            </button>
             <!-- Dropdown menu -->
             <div id="userSettings" class="z-10 hidden font-normal bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 dark:divide-gray-600">
               <ul class="py-2 text-sm text-gray-900" aria-labelledby="dropdownLargeButton">
