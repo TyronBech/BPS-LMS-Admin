349d3969fed8b232e9768813f292791ac736be11
TyronBech <tyronbechayda1112@gmail.com>
2025-11-25 23:48:02 +0800

    Add ApexCharts dependency and initialize in app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,7 +3,9 @@ import Chart from 'chart.js/auto';
 import ChartDataLabels from 'chartjs-plugin-datalabels';
 import $ from 'jquery';
 import axios from 'axios';
+import ApexCharts from 'apexcharts';
 
+window.ApexCharts = ApexCharts;
 window.axios = axios;
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 window.$ = window.jQuery = $;
