82a79cb36fd264f8b9f93400626441a4355eb836
TyronBech <tyronbechayda1112@gmail.com>
2026-02-11 20:25:32 +0800

    fix: Update remarks and availability fields to indicate readonly status with appropriate styling

--- resources/views/maintenance/books/create.blade.php
+++ resources/views/maintenance/books/create.blade.php
@@ -108,7 +108,7 @@
         </div>
         <div>
           <label for="remarks" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Remarks:</label>
-          <p class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:text-white">Missing</p>
+          <p class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 hover:cursor-not-allowed">Missing</p>
           <input type="hidden" id="remarks" name="remarks" value="Missing" readonly>
           @error('remarks')
           <p class="mt-2 text-sm text-red-600 dark:text-red-500">{{ $message }}</p>
@@ -127,7 +127,7 @@
         </div>
         <div>
           <label for="availability" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Availability:</label>
-          <p class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:text-white">Unavailable</p>
+          <p class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 hover:cursor-not-allowed">Unavailable</p>
           <input type="hidden" id="availability" name="availability" value="Unavailable" readonly>
           @error('availability')
           <p class="mt-2 text-sm text-red-600 dark:text-red-500">{{ $message }}</p>
