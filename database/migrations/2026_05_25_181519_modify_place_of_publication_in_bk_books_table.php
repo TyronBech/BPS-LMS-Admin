7738cf66a493bf7e592b7a9c47d7341d3259a375
TyronBech <tyronbechayda1112@gmail.com>
2026-05-25 18:18:27 +0800

    feat: allow nullable text for place_of_publication in bk_books table

---
+++ database/migrations/2026_05_25_181519_modify_place_of_publication_in_bk_books_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('bk_books', function (Blueprint $table) {
+            $table->text('place_of_publication')->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('bk_books', function (Blueprint $table) {
+            $table->string('place_of_publication', 50)->nullable()->change();
+        });
+    }
+};
